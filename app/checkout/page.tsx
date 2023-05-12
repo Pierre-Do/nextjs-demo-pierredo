80bd4c4b70c3558ced03b3ee28460c5ad395f304
Pierre-Dominique Putallaz <pierre-dominique.putallaz@qoqa.com>
2023-05-12 15:12:45 +0200

    Simple page navigation

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,17 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function Home(): React.JSX.Element {
+export default function Checkout(): React.JSX.Element {
   return (
     <main className="flex gap-2 p-24">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Add to Cart
-      </button>
       <Link
         className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-        href="/checkout"
+        href="/"
       >
-        Checkout
+        Back to Product
       </Link>
     </main>
   );
